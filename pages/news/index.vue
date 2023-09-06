209a8fc990ee5fea92b07fa753617d43e4dfaedd
Hibiki Kudo <h-kudo@cloudpack.jp>
2023-09-06 17:39:57 +0900

    お知らせページ作成

---
+++ pages/news/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <p>ニュース一覧ページ</p>
+      <div v-for="n in response.list" :key="n.slug">
+        <nuxt-link :to="`/news/${n.topics_id}`">
+          {{ n.ymd }} {{ n.subject }}
+        </nuxt-link>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    async asyncData({ $axios }) {
+      return {
+        response: await $axios.$get('/rcms-api/4/news'),
+      };
+    },
+  };
+  </script>
