25763af34f1b9fde915269da3e03b2502a952044
Hibiki Kudo <h-kudo@cloudpack.jp>
2023-09-12 15:00:05 +0900

    プレビュー機能実装

---
+++ pages/news_preview/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1 class="title">{{ response.details.subject }}</h1>
+      <div class="post" v-html="response.details.contents"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    async asyncData({ $axios, route }) {
+      return {
+        response: await $axios.$get(`/rcms-api/5/news/preview`, {
+          params: {
+            preview_token: route.query.preview_token,
+          },
+        }),
+      };
+    },
+  };
+  </script>
