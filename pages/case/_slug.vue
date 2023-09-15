96492fcb027d14a4806754e0ae7a30485bfb8499
Hibiki Kudo <h-kudo@cloudpack.jp>
2023-09-15 17:41:35 +0900

    APIとフロントエンドの繋ぎ込み

--- pages/case/_slug.vue
+++ pages/case/_slug.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
-    <h1 class="title">{{ response.details.subject }}</h1>
-    <div class="post" v-html="response.details.contents"></div>
+    <!-- <h1 class="title">{{ response.details.subject }}</h1> -->
 
-    <h2>{{ response.details.ext_1 }}</h2>
-    <img :src="response.details.ext_2.url" width="300">
-    <h3>{{ response.details.ext_3 }}</h3>
-    <p>{{ response.details.ext_4 }}</p>
-    <div>{{ response.details.ext_5[0] }}</div>
-    <div v-for="n in response.details.ext_5" :key="n.slug" >
-      <div>{{ n.ext_5 }}</div>
-      <div>{{ n.ext_6 }}</div>
-      <div>{{ n.ext_7 }}</div>
+    <h2>{{ response.details.case.case_title }}</h2>
+    <img :src="response.details.case.case_logo_img.url" width="150">
+    <img :src="response.details.case.case_main_img.url" width="600">
+    <h3>{{ response.details.case.case_lead }}</h3>
+    <p>{{ response.details.case.case_desc }}</p>
+     <div v-for="n in response.details.point" :key="n.slug" >
+      <h3>{{ n.point_title }}</h3>
+      <div>{{ n.point_lead }}</div>
+      <div>{{ n.point_desc }}</div>
+      <img :src="n.point_img.url" width="300">
+    </div>
+    <div v-for="n in response.details.category" :key="n.slug" >
+      <div>{{ n }}</div>
     </div>
   </div>
 </template>
-  
 <script>
 export default {
   async asyncData({ $axios, params }) {
