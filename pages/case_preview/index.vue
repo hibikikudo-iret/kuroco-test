38ebb16c1e07096c5a5b70b36cb6864bb3a8a50e
Hibiki Kudo <h-kudo@cloudpack.jp>
2023-09-19 18:05:15 +0900

    プレビュー機能

---
+++ pages/case_preview/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- <h1 class="title">{{ response.details.subject }}</h1> -->
+
+    <h2>{{ response.details.case.case_title }}</h2>
+    <img :src="response.details.case.case_logo_img.url" width="150">
+    <img :src="response.details.case.case_main_img.url" width="600">
+    <h3>{{ response.details.case.case_lead }}</h3>
+    <p>{{ response.details.case.case_desc }}</p>
+     <div v-for="n in response.details.point" :key="n.slug" >
+      <h3>{{ n.point_title }}</h3>
+      <div>{{ n.point_lead }}</div>
+      <div>{{ n.point_desc }}</div>
+      <img :src="n.point_img.url" width="300">
+    </div>
+    <div v-for="n in response.details.category" :key="n.slug" >
+      <div>{{ n }}</div>
+    </div>
+  </div>
+</template>
+  
+  <script>
+  export default {
+    async asyncData({ $axios, route }) {
+      return {
+        response: await $axios.$get(`/rcms-api/7/case_preview`, {
+          params: {
+            preview_token: route.query.preview_token,
+          },
+        }),
+      };
+    },
+  };
+  </script>
